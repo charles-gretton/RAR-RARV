b086a6b80e5fc829f5817030f5d8ab2e48ab9550
Charles Gretton <charles.gretton@nicta.com.au>
2013-07-10 11:40:29 +1000

    Generic "solve.sh" script, takes a single argument which is the name of the input .vrx formatted file.

--- 1000/solve.sh
+++ 1000/solve.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-indigo -c ./CONFIGS/configv C1_10_9.vrx mstWeight:0 convexHullAreaWeight:0 convexHullWeight:0 lollipopWeight:0  bendingEnergyWeight:0 lizziepopWeight:0 lollipopPct:100 lizziepopPct:100
+indigo -c ./CONFIGS/configv $1 mstWeight:0 convexHullAreaWeight:0 convexHullWeight:0 lollipopWeight:0  bendingEnergyWeight:0 lizziepopWeight:0 lollipopPct:100 lizziepopPct:100
