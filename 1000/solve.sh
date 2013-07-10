6245dd5ca1d8434ffebf3646c8148a548dcd97ce
Charles Gretton <charles.gretton@nicta.com.au>
2013-07-10 11:32:44 +1000

    Indigo solver configuration siles, and solve.sh invocation script for Indigo.

---
+++ 1000/solve.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+indigo -c ./CONFIGS/configv C1_10_9.vrx mstWeight:0 convexHullAreaWeight:0 convexHullWeight:0 lollipopWeight:0  bendingEnergyWeight:0 lizziepopWeight:0 lollipopPct:100 lizziepopPct:100
